ec8446dbaf18b74408cb609b1b55220835f578d0
PaulaMaribelBernalPe <PaulaMaribelBernalPe@172.22.114.16>
2026-09-21 15:49:47 -0500

    prueba class

--- src/main/java/com/mycompany/proyectos_java_pau/VECTORES.java
+++ src/main/java/com/mycompany/proyectos_java_pau/VECTORES.java
@@ -2,11 +2,11 @@ package com.mycompany.proyectos_java_pau;
 import java.util.Scanner;
 public class VECTORES {
 static Scanner sc = new Scanner ( System.in);
-static String [] name = new String[10];
+static String [] name = new String[10 ];
 static String [] surname = new String[10];
 static double [] salary = new double[10];
 static int [] dwork = new int[10];
-static double [] dpay = new double[10];
+static double [] tp = new double[10];
 static int opc;
 static int cantidad;
 static int pos; 
@@ -47,52 +47,64 @@ static int[] vec = new int[10];
         System.out.println("¿Cuántos empleados desea registrar?: ");
         cantidad =sc.nextInt();
         for(int i = 0; i < cantidad; i ++){
-            System.out.println("Digita el Nombre: ");
+            System.out.println("Digit the name: ");
             name[i]=sc.next();
-            System.out.println("Digita el Apellido: ");
+            System.out.println("Digit the surname: ");
             surname[i]=sc.next();
-            System.out.println("Digita el Salario: ");
+            System.out.println("Digit the salary: ");
             salary[i]=sc.nextDouble();
-            System.out.println("Digite los Días Trabajados: ");
+            System.out.println("Digit the days worked: ");
             dwork[i]=sc.nextInt();
-            System.out.println("Digite los Días Pagados: ");
-            dpay[i]=sc.nextDouble();
-        }
+            tp[i]= (salary[i]/30)*dwork[i];
+                    }
             menu(); 
     }
 
     
       public static void consultar(){
+            System.out.println("************");
+    System.out.println("         MENU CONSULTA          ");
+    System.out.println("************");
+    System.out.println("1. Consulta General");
+    System.out.println("2. Consulta Individual");
+    System.out.println("3. Regresar al Menu");
+    System.out.println("Digite su opcion ");
+    opc = sc.nextInt();
+    
+    switch(opc){
+        case 1:
+            for(int i = 0 ; i < cantidad;i++){
+                System.out.printf("Name   "+"Surname   "+"Salary   "+"Days worked   "+ "Total a pagar   ");
+                System.out.printf(name[pos]+   surname[pos]   +salary[pos]   +dwork[pos]   +tp[pos]);
+                }
+                    break;
+    }
+    
           do{System.out.println("Ingrese la posicion deseada: ");
           pos = sc.nextInt();
-              System.out.println("Nombre: "+ name[pos]);
-              System.out.println("Apellido: "+ surname[pos]);
-              System.out.println("Salario: "+ salary[pos]);
-              System.out.println("Días Trabajados: "+ dwork[pos]);
-              System.out.println("Días Pagados: "+ dpay[pos]);
-              System.out.println("¿Necesitas otra consulta?     1 = Sí    0 = No");
+              System.out.printf("Name   "+"Surname   "+"Salary   "+"Days worked   "+ "Total a pagar   ");
+                System.out.printf(name[pos]+   surname[pos]   +salary[pos]   +dwork[pos]   +tp[pos]);
+              System.out.println("¿Need another consultation?     1 = Sí    0 = No");
               des = sc.nextInt();
           }while(des != 0);
-          menu();
+          consultar();
           }
     
         public static void modificar(){
             do{System.out.println("¿Que posicion quieres editar?: ");
             pos = sc.nextInt();
-            System.out.println("Nombre: ");
+            System.out.printf("Nombre: ");
             name[pos]=sc.next();
-              System.out.println("Apellido: ");
+              System.out.printf("  Apellido: ");
               surname[pos]= sc.next();
-              System.out.println("Salario: ");
+              System.out.printf("  Salario: ");
               salary[pos]= sc.nextDouble();
-              System.out.println("Días Trabajados: ");
+              System.out.printf("  Días Trabajados: ");
               dwork[pos]= sc.nextInt();
-              System.out.println("Días Pagados: ");
-              dpay[pos]= sc.nextDouble();
-              System.out.println("¿Necesitas otra consulta?     1 = Sí    0 = No");
+              System.out.println("¿Need another consultation?     1 = Sí    0 = No");
               des = sc.nextInt();
             }while(des != 0);
-            menu();
+            modificar();
     }
 
           public static void eliminar(){
@@ -104,18 +116,16 @@ static int[] vec = new int[10];
                   surname[i] = surname[i + 1];
                   salary[i] = salary[i + 1];
                   dwork[i] = dwork[i + 1];
-                  dpay[i] = dpay[i + 1];
               }
               cantidad --; 
               name[cantidad] = null;
               surname[cantidad] = null;
               salary[cantidad] = 0;
               dwork[cantidad] = 0;
-              dpay[cantidad] = 0;
                System.out.println("¿Deseas borrar otro registro?     1 = Si   0 = No");
                   des = sc.nextInt();
                   
               }while(des != 0);
-              menu();
+              eliminar();
     }
 }
